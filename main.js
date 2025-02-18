03bc300e1d89a7a654e64fc748db1e6f373cd3f7
josué <josuebeurensilvaa@gmail.com>
2025-02-18 15:54:52 -0300

    second commit

--- main.js
+++ main.js
@@ -1,67 +1,72 @@
 $(document).ready(function () {
+    // Inicializando o carrossel
     $('#carousel-imagens').slick({
         autoplay: true,
-        arrows: false
-    })
-})
+        arrows: false,
+        dots: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+        speed: 500,
+    });
 
-$('#telefone').mask('(00) 00000-0000', {
-    placeholder: '(DDD) 12345-6789'
-})
+    // Máscaras de campos
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(DDD) 12345-6789'
+    });
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '123.456.789-00'
+    });
+    $('#cep').mask('00000-000', {
+        placeholder: '01234-567'
+    });
 
-$('#cpf').mask('000.000.000-00', {
-    placeholder: '123.456.789-00'
-})
-
-$('#cep').mask('00000-000', {
-    placeholder: '012345-678'
-})
-
-
-
-
-
-$('form').validate({
-    rules: {
-        nome: {
-            required: true
+    // Validação do formulário
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            },
         },
-        email: {
-            required: true,
-           
+        messages: {
+            nome: 'Por favor, insira seu nome',
+            email: 'Por favor, insira um email válido',
+            telefone: 'Por favor, insira seu telefone',
+            cpf: 'Por favor, insira seu CPF',
+            endereco: 'Por favor, insira seu endereço',
+            cep: 'Por favor, insira seu CEP',
         },
-        telefone: {
-            required: true
-        },
-        cpf: {
-            required: true
-        },
-    },
-    
+        submitHandler: function (form) {
+            event.preventDefault();
 
-    messages : {
-        nome: 'Por favor, insira seu nome',
-          email: 'Por favor, insira seu email',
-          telefone: 'Por favor, insira seu telefone',
-            cpf :'Por favor, insira seu CPF',
-    },
-    submitHandler: function (form) {
-        event.preventDefault();
-    
-    
-        $("#mensagem")
-            .html("Formulário enviado com sucesso!")
-            .fadeIn()
-            .fadeOut(5000)
-            .delay(5000)  
+            $("#mensagem")
+                .html("Formulário enviado com sucesso!")
+                .fadeIn()
+                .fadeOut(5000)
+                .delay(5000);
 
             $("#form")[0].reset();
-    },
-
-    invalidHandler: function(evento, validador){
-        let camposIncorretos = validador.numberOfInvalids();
-       if (camposIncorretos){
-        alert(`Existem ${camposIncorretos} campos incorretos`)
-       }
-    }
-})
+        },
+        invalidHandler: function (evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`);
+            }
+        }
+    });
+});
